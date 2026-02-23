00410c22b714897d2b62b266f9946c9269524b7f
pumpedkk <130869255+pumpedkk@users.noreply.github.com>
2026-02-23 12:42:44 -0300

    subtitle reference in extension

--- Runtime/SubtitleExtension.cs
+++ Runtime/SubtitleExtension.cs
@@ -1,6 +1,12 @@
 using UnityEngine;
+using GGTools.Subtitle;
 
 public abstract class SubtitleExtension : MonoBehaviour
 {
+    private Subtitle subtitle;
+    public virtual void Reset()
+    {
+        subtitle = GetComponent<Subtitle>();
+    }
 
 }
